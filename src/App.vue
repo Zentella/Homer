14db14bb53683cd2e64f412efa44e316831ad090
mauricio <mauricio.vera.r7@gmail.com>
2021-09-10 08:40:24 -0300

    ver.1

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <span>{{ message }}</span>
+    <img
+      src="https://i.pinimg.com/originals/54/67/54/5467549d4d0c67f326e792d5803
+bcc16.png"
+    />
+    <!--   envia datos de padre a hijo -->
+<HelloVue @clickedResponse="clickChild" message="Ustedes son mis hijos..."></HelloVue>
+<!--   <HelloVue message="Ustedes son mis hijos..."></HelloVue> -->
+<!--   recibe datos  del hijo -->
+<span class="response"> {{childResponse}}</span>    
+<!--   <span class="response"> {{this.childResponse}} no poner this porque es global</span> -->
+
+  </div>
+</template>
+
+<script>
+import HelloVue from "./HelloVue.vue";
+
+export default {
+  name: "app",
+  components: { HelloVue },
+  data() {
+    return {
+      message: "Hola soy Homero",
+      childResponse: ''
+    };
+  },
+  methods: {
+clickChild(childValue) {
+this.childResponse = childValue;
+}
+}
+};
+</script>
+
+<style scoped>
+#app {
+  display: flex;
+flex-flow: column nowrap;
+  font-size: 38px;
+  font-family: "Calibri", sans-serif;
+  color: darkblue;
+  background: lightblue;
+}
+img {
+  width: 25%;
+  height: 25%;
+}
+.response {
+color: red;
+}
+
+</style>
---
+++ src/HelloVue.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hellovue">
+    {{ message }}
+    <img
+      class="helloimg"
+      src="https://images.vectorhq.com/images/previews/1ba/bart-and-lisa-psd-4
+46281.png"
+    />
+    <button class="hello-response" @click="clickResponse">Responder</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "hellovue",
+  props: {
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    clickResponse(event) {
+      this.$emit("clickedResponse", "Ay! Caramba !!!");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.hellovue {
+  display: flex;
+  flex-flow: column nowrap;
+  padding: 100px 0 0 0;
+}
+.helloimg {
+  width: 25%;
+  height: 25%;
+}
+.hello-response {
+width: 20%;
+color: red;
+}
+
+</style>
